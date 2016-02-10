9c3d4e20b3dae419242f855d9c2256ace10864ab
--global <--global>
2016-02-10 23:51:30 +0200

    Second React app lesson
    created simple list manager

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,22 +1,14 @@
 var React = require('react');
-var ListItem = require('./ListItem.jsx');
+var ListItem = require('./Listitem.jsx');
 
-var ingredients = [
-  {"id":1,"text":"ham"},
-  {"id":2,"text":"cheese"},
-  {"id":3,"text":"potatose"}
-]
 var List = React.createClass({
   render: function() {
-    var listItems = ingredients.map(function(item) {
-      return (
-        <ListItem key={item.id} ingredient={item.text} />
-      )
-    });
+    var createItem = function(text, index) {
+      return <ListItem key = {index + text} text = {text} />;
+    };
     return (
-      <ul>{listItems}</ul>
-    );
-
+      <ul>{this.props.items.map(createItem)}</ul>
+    )
   }
 });
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 var React = require('react');
 var ReactDom = require('react-dom');
-var List = require('./components/List.jsx');
+var ListManager = require('./components/ListManager.jsx');
 
-ReactDom.render(<List />, document.getElementById('ingredients'));
+ReactDom.render(<ListManager title="Ingredients" />, document.getElementById('ingredients'));
